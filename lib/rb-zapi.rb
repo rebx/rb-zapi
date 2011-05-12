8184d66e68879fae18dcd548101046f2bf009f20
Rebs Guarina <rebs.guarina@gmail.com>
2011-05-12 22:34:44 +1200

    code cleanup. added accessors

--- lib/rb-zapi.rb
+++ lib/rb-zapi.rb
@@ -2,13 +2,14 @@
 require 'rb-zapi/zapidef'
 
 class RbZapi
-  
+
   include ZapiDefinition
-  
-  attr_accessor :server, :na_user, :na_passwd
-  
+
+  attr_accessor :server, :na_user, :na_passwd, :auth_style, :proto_trans
+  attr_reader :zapi_uri
+
   def initialize(server)
     @server = server
   end
-  
+
 end
