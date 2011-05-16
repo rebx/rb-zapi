811ecaec2909eff2dfb841e8d3ee90dfa65caeb1
Rebs Guarina <rebs.guarina@gmail.com>
2011-05-16 21:56:58 +1200

    Moved methods from zapidef to zapimeth

--- lib/rb-zapi/zapidef.rb
+++ lib/rb-zapi/zapidef.rb
@@ -1,45 +1,37 @@
 
+# encoding: utf-8
+
 module ZapiDefinition
 
+  # ZAPI version standards
   MAJORVERS = 1
   MINORVERS = 3
+  # User defaults
   DEF_NA_USER = 'root'
   DEF_NA_PASSWD = ''
+  # Authentication style
   # LOGIN OR HOSTS OR LOGIN_PASSWORD
   AUTH_STYLE = 'LOGIN'
-  SERVER_TYPE = 'FILER'
+  SERVER_TYPE = 'Filer'
+  # Protocol transport
   # HTTP or NA_SERVER_TRANSPORT_HTTPS
   PROTO_TRANSPORT = 'HTTP'
+  # XML header stuff
   NA_NAMESPACE_URI = 'http://www.netapp.com/filer/admin'
   DEF_DOCTYPE = 'file:/etc/netapp_filer.dtd'
+  # Filer to URI Mappings
   TYPE_URL_HASH = {
-    Filer     =>   "/servlets/netapp.servlets.admin.XMLrequest_filer",
-    NetCache  =>   "/servlets/netapp.servlets.admin.XMLrequest",
-    Agent     =>   "/apis/XMLrequest",
-    DFM       =>   "/apis/XMLrequest"
+    'Filer'     =>   "/servlets/netapp.servlets.admin.XMLrequest_filer",
+    'NetCache'  =>   "/servlets/netapp.servlets.admin.XMLrequest",
+    'Agent'     =>   "/apis/XMLrequest",
+    'DFM'       =>   "/apis/XMLrequest"
   }
+  # Filer to port Mappings
   TYPE_PORT_HASH = {
-    Filer     =>    80,
-    NetCache  =>    80,
-    Agent     =>    4092,
-    DFM       =>    8081
+    'Filer'     =>    80,
+    'NetCache'  =>    80,
+    'Agent'     =>    4092,
+    'DFM'       =>    8081
   }
-
-  def set defaults
-
-  end
-
-  def set_auth_style(auth_style=AUTH_STYLE)
-
-  end
-
-  def set_proto_trans(proto_trans=PROTO_TRANSPORT)
-
-  end
-
-  def set_server_type(server_type=SERVER_TYPE)
-
-  end
-
 
 end
