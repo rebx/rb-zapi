28a77d0760623b42bea19261b916f4b16a76d830
Rebs Guarina <rebs.guarina@gmail.com>
2011-05-16 20:35:50 +1200

    Added accessor for server_type. Initialized more parameters

--- lib/rb-zapi.rb
+++ lib/rb-zapi.rb
@@ -5,11 +5,16 @@ class RbZapi
 
   include ZapiDefinition
 
-  attr_accessor :server, :na_user, :na_passwd, :auth_style, :proto_trans
-  attr_reader :zapi_uri
+  attr_accessor :host, :server, :na_user, :na_passwd, :auth_style, :proto_trans, :server_type
+  attr_reader :zapi_uri, :port
 
-  def initialize(server)
+  def initialize(server, na_user, na_passwd, auth_style, proto_trans, server_type)
     @server = server
+    @na_user = na_user
+    @na_passwd = na_passwd
+    set_auth_style(auth_style)
+    set_proto_trans(proto_trans)
+    set_server_type(server_type)
   end
 
 end
