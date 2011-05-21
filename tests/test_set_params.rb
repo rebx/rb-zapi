d0885949b1d5322516a6f1e230a5d2f70bfbb2cf
Rebs Guarina <rebs.guarina@gmail.com>
2011-05-21 20:34:08 +1200

    Added more tests

--- tests/test_set_params.rb
+++ tests/test_set_params.rb
@@ -1,6 +1,7 @@
 
 require 'shoulda'
-require_relative '../lib/rb-zapi/zapimeth'
+require_relative '../lib/rb-zapi/zapi_methods'
+require_relative '../lib/rb-zapi/zapi_definition'
 
 class TestSetParams < Test::Unit::TestCase
 
@@ -11,40 +12,65 @@ class TestSetParams < Test::Unit::TestCase
       @obj.extend(ZapiMethods)
     end
 
+    # auth_style tests
     should "have authentication style defined when style is given" do
       style = 'LOGIN_PASSWORD'
       @obj.set_auth_style(style)
       assert @obj.instance_variable_defined?("@auth_style")
     end
 
+    should "return authentication style when no style is given" do
+      @obj.set_auth_style()
+      assert_equal @obj.instance_variable_get("@auth_style"), ZapiDefinition::AUTH_STYLE
+    end
+    
     should "return authentication style when style is given" do
       style = 'LOGIN_PASSWORD'
       @obj.set_auth_style(style)
       assert_equal @obj.instance_variable_get("@auth_style"), style
     end
 
+    # proto_trans tests
     should "have protocol transport defined when it is given" do
       proto = 'NA_SERVER_TRANSPORT_HTTPS'
       @obj.set_protocol_transport(proto)
       assert @obj.instance_variable_defined?("@proto_trans")
     end
 
+    should "return protocol transport when none is given" do
+      @obj.set_protocol_transport()
+      assert_equal @obj.instance_variable_get("@proto_trans"), ZapiDefinition::PROTO_TRANSPORT
+    end
+    
     should "return protocol transport when it is given" do
       proto = 'NA_SERVER_TRANSPORT_HTTPS'
       @obj.set_protocol_transport(proto)
       assert_equal @obj.instance_variable_get("@proto_trans"), proto
     end
 
+    # server_type tests
     should "have server type when it is given" do
       type = 'Filer'
       @obj.set_server_type(type)
       assert @obj.instance_variable_defined?("@server_type")
+    end
+    
+    should "return server type when none is given" do
+      @obj.set_server_type()
+      assert_equal @obj.instance_variable_get("@server_type"), ZapiDefinition::SERVER_TYPE
     end
 
     should "return server type when it is given" do
       type = 'Filer'
       @obj.set_server_type(type)
       assert_equal @obj.instance_variable_get("@server_type"), type
+    end
+    
+    # port definition test
+    
+    should "have port when it is given" do
+      port = 80
+      @obj.set_server_type
     end
   end
 
